023be29d2ac9ee32824fb54cacced4159aca468f
Srijan668 <dassrijan339@gmail.com>
2024-08-21 12:34:53 +0530

    Update Assignment1.2.cpp

--- Assignment1.2.cpp
+++ Assignment1.2.cpp
@@ -1,16 +1,16 @@
 #include<iostream>
 int main()
 {
-double x,y;
-std::cout<<"enter number\n";
-std::cin>>x;
-if(x==0.0)
-{
-std::cout<<"change x";
-}
-else
-{
-y=x+1/(x+(1/(x+1/x)));
-std::cout<<"the ans is "<< y <<"\n";
-}
+ double x,y;
+ std::cout<<"enter number\n";
+ std::cin>>x;
+ if(x==0.0)
+ {
+   std::cout<<"change x";
+ }
+ else
+ {
+   y=x+1/(x+(1/(x+1/x)));
+   std::cout<<"the ans is "<< y <<"\n";
+ }
 }
