3a2393952f0c6b0196584a0f955b4d2d1ceb4061
Srijan668 <dassrijan339@gmail.com>
2024-08-21 12:58:32 +0530

    Update Assignment1.3.2.cpp

--- Assignment1.3.2.cpp
+++ Assignment1.3.2.cpp
@@ -1,10 +1,14 @@
 #include<iostream>
 int main()
 {
-float x,y;
-std::cout<<"enter the y";
-std::cin>>y;
-x=y-20.0*6.0/9.0;
-std::cout<<"x="<<x<<"\n";
+ float x,y;
+ std::cout<<"enter the y";
+ std::cin>>y;
+ x=y-20*6/9;
+ std::cout<<"x="<<x<<"\n";
+ //FOR Y=1 X=-12 .IT IS NOT THE EXPECTED ANS
+ x=y-20.0*6.0/9.0;
+ std::cout<<"CORRECT x="<<x<<"\n";
+ // FOR Y=1 X=-12.3333
 return 0;
 }
