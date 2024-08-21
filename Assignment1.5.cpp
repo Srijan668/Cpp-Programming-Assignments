207d0cde2b3951c9077a71d5a71cfc203964b24d
Srijan668 <dassrijan339@gmail.com>
2024-08-21 13:01:11 +0530

    Update Assignment1.5.cpp

--- Assignment1.5.cpp
+++ Assignment1.5.cpp
@@ -6,12 +6,12 @@ int main()
 	float n,nn,p;
 	std::cout<<"enter the number";
 	std::cin>>n;
-	std::cout<<"enter the kth place";
+	std::cout<<"enter the kth place ";
 	std::cin>>k;
-	nn=n*pow(10,k);
+	nn=n*k;
 	nn=nn+0.5;
 	m=(int)nn;
-	p=(float)m/pow(10,k);
+	p=(float)m/k;
 	std::cout<<"the ans is "<<p<<"\n";
 	return 0;
 }
