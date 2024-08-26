283cce41d2f6395dddc35236d034b079b710fa64
Srijan668 <dassrijan339@gmail.com>
2024-08-26 17:20:06 +0530

    Update Assignment1.5.cpp

--- Assignment1.5.cpp
+++ Assignment1.5.cpp
@@ -2,16 +2,17 @@
 #include<math.h>
 int main()
 {
-	int k,m;
-	float n,nn,p;
+	long int k,m;
+	long double n,nn,p;
 	std::cout<<"enter the number";
 	std::cin>>n;
 	std::cout<<"enter the kth place ";
 	std::cin>>k;
 	nn=n*k;
 	nn=nn+0.5;
-	m=(int)nn;
-	p=(float)m/k;
+	m=(long int)nn;
+	p=(long double)m/k;
+	std::cout<<std::fixed;
 	std::cout<<"the ans is "<<p<<"\n";
 	return 0;
 }
